e477656fa71028d932d58291f507d5189df839b4
Federico Antoniazzi <federico.antoniazzi.000@gmail.com>
2021-05-26 01:05:37 +0200

    Add the AWS Thing used to transfer data

---
+++ modules/iot/main.tf
@@ -0,0 +1,3 @@
+resource "aws_iot_thing" "siouxsilos_thing" {
+  name = var.siouxsilos_thing_name
+}
---
+++ modules/iot/variables.tf
@@ -0,0 +1,5 @@
+variable "siouxsilos_thing_name" {
+  description = "Name of the thing used to transfer silos data"
+  type        = string
+  default     = "clod2021-g5pw-silos"
+}
